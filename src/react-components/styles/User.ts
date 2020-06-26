18977f9d3b635ace79295a08b4d00165a8ea5675
Angelo Zambrano <angelozam17@gmail.com>
2020-06-25 19:27:20 -0500

    --amend

--- src/react-components/styles/User.ts
+++ src/react-components/styles/User.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const UserStyled = styled.div`
    width: 100%;
-   height: 100vh;
+   min-height: 100vh;
    margin: 0 auto;
    background-color: var(--bg);
 
